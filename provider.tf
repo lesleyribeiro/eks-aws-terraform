d51d82f01ec469327ebf4677ce8f8addac0d1bd2
lesleyribeiro <lesleysribeiro@gmail.com>
2024-10-01 14:43:47 -0300

    fix: lint

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.69.0"
+    }
+  }
+  backend "s3" {
+    bucket = "lab-eks-tfstate"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
